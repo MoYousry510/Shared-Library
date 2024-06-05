f7337e080699bb3d5164641caef853ef710b9f34
Mohammed Yousry <80543993+MoYousry510@users.noreply.github.com>
2024-06-05 15:46:41 +0300

    Update deployOnOpenShift.groovy

--- vars/deployOnOpenShift.groovy
+++ vars/deployOnOpenShift.groovy
@@ -9,7 +9,6 @@ def call(String OpenShiftCredentialsID, String openshiftClusterurl, String opens
     // login to OpenShift Cluster via cluster url & service account token
     withCredentials([string(credentialsId: "${OpenShiftCredentialsID}", variable: 'OpenShift_CREDENTIALS')]) {
             sh "oc login --server=${openshiftClusterurl} --token=${OpenShift_CREDENTIALS} --insecure-skip-tls-verify"
-            sh "oc project ${openshiftProject}"
             sh "oc apply -f ."
     }   
 }
